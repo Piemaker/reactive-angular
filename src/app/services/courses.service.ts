32def7a0f53d9c761be2a34b55b661e825827509
Omar Sayed <orhman@hotmail.com>
2024-04-22 16:32:06 +0200

    fix: :zap: prevent multiple http requests by using shareReplay

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -13,8 +13,9 @@ export class CoursesService {
   // 1- Services return only observables
   // 2- Services don't contain state
   loadAllCourses(): Observable<Course[]> {
-    return this.http
-      .get<Course[]>("/api/courses")
-      .pipe(map((data) => data["payload"]));
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((data) => data["payload"]),
+      shareReplay()
+    );
   }
 }
